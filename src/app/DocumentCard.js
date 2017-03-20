f87c5cc0c305fcbb93b4ea6a5c012317cb8b76ba
Milán Jakab Gulyás <gumija6@gmail.com>
2017-03-20 16:07:18 +0100

    stopPropagation trial, open document button

--- src/app/DocumentCard.js
+++ src/app/DocumentCard.js
@@ -3,6 +3,8 @@ import { Card, CardActions, CardTitle, CardText } from 'material-ui/Card';
 import Divider from 'material-ui/Divider';
 import FlatButton from 'material-ui/FlatButton';
 import FontIcon from 'material-ui/FontIcon';
+import RaisedButton from 'material-ui/RaisedButton';
+import TouchRipple from 'material-ui/internal/TouchRipple';
 import './DocumentCard.css';
 
 export default class DocumentCard extends Component {
@@ -10,21 +12,25 @@ export default class DocumentCard extends Component {
   render() {
     return (
       <Card className="card">
-        <CardTitle
-          title={this.props.document.title}
-        />
-        <CardText>
-          {this.props.document.preview}
-        </CardText>
-        <Divider />
-        <CardActions className="card-actions">
-          <FlatButton
-            icon={<FontIcon className="material-icons">share</FontIcon>}
+          <CardTitle
+            title={this.props.document.title}
           />
-          <FlatButton
-            icon={<FontIcon className="material-icons">delete</FontIcon>}
-          />
-        </CardActions>
+          <CardText>
+            {this.props.document.preview}
+          </CardText>
+          <Divider />
+          <CardActions className="card-actions">
+            <FlatButton
+              icon={<FontIcon className="material-icons">share</FontIcon>}
+              onTouchTap={(e) => { e.stopPropagation(); alert('icon'); }}
+            />
+            <FlatButton
+              icon={<FontIcon className="material-icons">delete</FontIcon>}
+            />
+            <RaisedButton>
+              OPEN
+            </RaisedButton>
+          </CardActions>
       </Card>
     );
   }
