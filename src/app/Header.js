261817bac0bce05549725dc8e2f0d3dafc7e6b8e
Milán Jakab Gulyás <gumija6@gmail.com>
2017-04-01 10:48:11 +0200

    Fixing double appear

--- src/app/Header.js
+++ src/app/Header.js
@@ -19,32 +19,42 @@ import { inject, observer } from 'mobx-react';
 @observer
 export default class Header extends Component {
 
+  componentWillMount() {
+    console.log('Header props:', this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('Header props:', nextProps);
+  }
+
   render() {
     return (
-      <div>
-        <Route exact path="/" children={() =>
-          <AppBar
-            title='Wimer'
-            iconElementLeft={
-              <Link to={`/document/1`}><IconButton><MenuIcon /></IconButton></Link>
-            } />
-        } />
-        <Route path="/document/:id" children={(props) =>
-          <AppBar
-            // eslint-disable-next-line
-            title={props.match ? this.props.documentStore.docInfos.find((d) => d.id == props.match.params.id).title : "NULL"}
-            iconElementLeft={
-              <IconButton onTouchTap={() => props.history.goBack()}><BackIcon /></IconButton>
-            }
-            iconElementRight={
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <Versions />
-                <Share />
-                <UserManagement />
-              </div>
-            } />
-        } />
-      </div>
+        <div>
+          <Route exact path="/" render={() =>
+            <AppBar
+              title='Wimer'
+              iconElementLeft={
+                <Link to={`/document/1`}><IconButton><MenuIcon /></IconButton></Link>
+              } 
+              style={{position: 'fixed', top: 0}}/>
+          } />
+          <Route exact path="/document/:id" render={(props) =>
+            <AppBar
+              // eslint-disable-next-line
+              title={props.match ? this.props.documentStore.docInfos.find((d) => d.id == props.match.params.id).title : "NULL"}
+              iconElementLeft={
+                <IconButton onTouchTap={() => props.history.goBack()}><BackIcon /></IconButton>
+              }
+              iconElementRight={
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  <Versions />
+                  <Share />
+                  <UserManagement />
+                </div>
+              }
+              style={{position: 'fixed', top: 0}} />
+          } />
+        </div>
     );
   }
 }
